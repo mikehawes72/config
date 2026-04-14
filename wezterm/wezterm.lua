2e8e3f19e1bd143f8a3bce4159c43d3ca44f0d9d
Mike Hawes <mhawes@aqa.org.uk>
2026-04-14 16:09:27 +0100

    Adds wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+--config.initial_cols = 120
+--config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 10
+config.color_scheme = "Tokyo Night"
+
+config.keys = {
+	{
+		key = "w",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+}
+
+-- Finally, return the configuration to wezterm:
+return config
