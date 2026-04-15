8ec9db2da0793bf4fd9d1b66f66ce5ffeff00cda
Mike Hawes <mike@mikehawes.me.uk>
2026-04-15 20:41:42 +0100

    Changes the initial font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,7 +11,7 @@ local config = wezterm.config_builder()
 --config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 10
+config.font_size = 12
 config.color_scheme = "Tokyo Night"
 
 config.keys = {
